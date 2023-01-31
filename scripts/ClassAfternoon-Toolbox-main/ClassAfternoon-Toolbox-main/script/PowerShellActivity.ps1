9b86ff35bf9fbb83d8dca05a1896a834046c64a5
Del Rosario <jericho.delrosario@fujitsu.com>
2023-01-31 11:52:32 +0800

    Edited PowerShellActivity.ps1

--- scripts/ClassAfternoon-Toolbox-main/ClassAfternoon-Toolbox-main/script/PowerShellActivity.ps1
+++ scripts/ClassAfternoon-Toolbox-main/ClassAfternoon-Toolbox-main/script/PowerShellActivity.ps1
@@ -1,12 +1,16 @@
-﻿Function ClearValues{
-    $Data = Get-Content -path "C:\Users\DelRosarioJe\Documents\Test\Test\scripts\Activity\create.csv"
+﻿$Data = Get-Content -path "C:\Users\DelRosarioJe\Documents\Test\Test\scripts\Activity\create.csv"
+Function ClearValues{
     $Data = $Data | Where {$_ -eq $Data[0]}
-    $Data | Out-File -FilePath "C:\Users\DelRosarioJe\Documents\Test\Test\scripts\Activity\create4.csv" -Encoding ascii
+    $Data # | Out-File -FilePath "C:\Users\DelRosarioJe\Documents\Test\Test\scripts\Activity\create.csv" -Encoding ascii
 }
 
+$Data1 = Import-Csv -Path "C:\Users\DelRosarioJe\Documents\Test\Test\scripts\Activity\create.csv"
 Function CheckMissing{
-    $Data = Import-Csv -Path "C:\Users\DelRosarioJe\Documents\Test\Test\scripts\Activity\create.csv"
     If($Data.Length -lt 1){
         Exit
     }
+    Else{
+        Continue
+    }
 }
+
