37574dd8ef4abe26c03779a40a2a367560019da1
Del Rosario <jericho.delrosario@fujitsu.com>
2023-02-14 01:50:58 +0800

    modified toolbox.ps1 and created Read.ps1

--- scripts/ClassAfternoon-Toolbox-main/ClassAfternoon-Toolbox-main/script/toolbox.ps1
+++ scripts/ClassAfternoon-Toolbox-main/ClassAfternoon-Toolbox-main/script/toolbox.ps1
@@ -13,7 +13,38 @@ try {
         $global:WarningIcon = [System.Windows.MessageBoxImage]::Warning
         $global:ErrorIcon = [System.Windows.MessageBoxImage]::Error
         $global:QButton = [System.Windows.MessageBoxImage]::Question
+        $objIcon = New-Object system.drawing.icon ("$RootPath\teamwork.ico")
+        $GifFile_CompBackground= (Get-Item -Path "$RootPath\gears.gif")
 
+        function ShowScreen {
+            param (
+                $Control
+            )
+
+            if ($Control -eq "TOA") {
+                $btnStart.Show()
+                $lblTOA.Show()
+                $cbxTOA.Show()
+                $lblMainMenu.Hide()
+                $shpDivider.Hide()
+                $btnCreate.Hide()
+                $btnRead.Hide()
+                $btnUpdate.Hide()
+                $btnDelete.Hide()
+            }elseif ($Control -eq "MainMenu") {
+                $btnStart.Hide()
+                $lblTOA.Hide()
+                $cbxTOA.Hide()
+                $lblMainMenu.Show()
+                $shpDivider.Show()
+                $btnCreate.Show()
+                $btnRead.Show()
+                $btnUpdate.Show()
+                $btnDelete.Show()
+            }
+            else{}
+            
+        }
     
     function global:Get-Kill {
         param (
@@ -27,11 +58,13 @@ try {
             Write-Output "`n`n------------------END ROOT-------------------------"
             Stop-Transcript | Out-Null
             ClearCreateCSV
+            DisConEXO
             exit
         }else{
             Write-Output "`n`n------------------END ROOT-------------------------"
             Stop-Transcript | Out-Null
             ClearCreateCSV
+            DisConEXO
             exit
         }
         
@@ -43,12 +76,90 @@ try {
         Set-Content $RootPath\create.csv 'Name,Purpose,Members'    
     }
 
+    function SetToolMode {
+        param (
+            $Mode
+        )
+        If($Mode -eq "Enabled")
+        {
+            $btnCreate.enabled = $true
+            $btnRead.enabled = $true
+            $btnUpdate.enabled = $true
+            $btnDelete.enabled = $true
+        }else{
+            $btnCreate.enabled = $false
+            $btnRead.enabled = $false
+            $btnUpdate.enabled = $false
+            $btnDelete.enabled = $false
+        }
+    }
+
+    #---init>m365 Util
+    function global:ConEXO {
+
+        try {
+            Connect-ExchangeOnline | Out-Null
+            Write-Host "$(Get-Date -Format "HH:mm")[Log]: EXO connected success"
+        }
+        catch {
+            Write-Host "$(Get-Date -Format "HH:mm")[Error]: EXO connected failed"
+            Get-Kill -Mode "Hard"
+        }
+        
+    }
+    function global:DisConEXO {
+
+        try {
+            Disconnect-ExchangeOnline -Confirm:$false | Out-Null
+            Write-Host "$(Get-Date -Format "HH:mm")[Log]: EXO disconnected success"
+        }
+        catch {
+            Write-Host "$(Get-Date -Format "HH:mm")[Error]: EXO disconnected  failed"
+        }
+        
+    }
+
+    function global:GetDL {
+        param (
+            $Identity
+        )
+        try {
+            Get-DistributionGroup -Identity $Identity 
+        }
+        catch {
+        }
+
+        return
+        
+    }
+
     Start-Transcript -Path "$RootPath\Toolbox_localtime_$(Get-Date -Format "MMddyyyyHHmm").txt" | Out-Null
     
     Write-Output "`n`n------------------BEGIN ROOT-------------------------"
     Write-Output "$(Get-Date -Format "HH:mm")[Log]: Form init success"
     
+    $ConResult = [System.Windows.MessageBox]::Show("Do you want to connect to EXO?","$($json.ToolName) $($json.ToolVersion)",$YesNoButton,$QButton)
+    
+    If($ConResult -eq "Yes")
+    {
+        ConEXO | Out-Null
+        $EXOStatus = "(Online)"
+    }else{
+        $EXOStatus = "(Offline)"
+        Write-Host "$(Get-Date -Format "HH:mm")[Log]: EXO connection skipped"
+    }
+
+
+
     #--form
+    $IMG_CompBackground = [System.Drawing.Image]::fromfile($GifFile_CompBackground)
+    $Gif_CompBackground = New-Object Windows.Forms.picturebox -Property @{
+        Location = New-Object System.Drawing.Point(185,275)
+        AutoSize = $true
+        Size = New-Object System.Drawing.Size(120,120)
+        SizeMode = [System.Windows.Forms.PictureBoxSizeMode]::StretchImage
+        Image = $IMG_CompBackground
+    }
     #---form-assembly
     $form = New-Object Windows.Forms.Form -Property @{
         Size = New-Object System.Drawing.Size(485,460)
@@ -56,6 +167,7 @@ try {
         StartPosition = 'CenterScreen'
         BackColor = $json.ToolUIBackColor
         FormBorderStyle = 'Fixed3D'
+        Icon = $objIcon
     }
     $lblTOA = New-Object System.Windows.Forms.Label -Property @{
         Location = New-Object System.Drawing.Point(85,50)
@@ -65,6 +177,12 @@ try {
         Text = "$($json.ToolTOAText)"
     }
 
+    $cbxTOA = New-Object System.Windows.Forms.Checkbox -Property @{
+        Location = New-Object System.Drawing.Point(90,200)
+        Size = New-Object System.Drawing.Size(500,20)
+        ForeColor = $json.ToolUILabelColorDark
+        Text = "Do not show this again"
+    }
 
     $btnStart = New-Object System.Windows.Forms.Button -Property @{
         Location = New-Object System.Drawing.Point(160,240)
@@ -76,31 +194,15 @@ try {
 
     $btnStart.Add_Click({
         Write-Host "$(Get-Date -Format "HH:mm")[Log]: TOA confirm success"
-        $btnStart.Hide()
-        $lblTOA.Hide()
-        $form.Controls.Add($shpDivider)
-        $form.Controls.Add($lblMainMenu)
-        $form.Controls.Add($btnCreate)
-        $form.Controls.Add($btnRead)
-        $form.Controls.Add($btnUpdate)
-        $form.Controls.Add($btnDelete)
-        $form.Controls.Add($box)
+        #TOA hide?
+        If ($cbxTOA.Checked -eq $true)
+        {
+            $json.ToolShowTOA = "False"
+            $json | ConvertTo-Json | Out-File "$RootPath\config.json"
+        }else{}
+
+        ShowScreen -Control "MainMenu"
     })
-
-    $InsertIcon = New-Object System.Drawing.Icon "$RootPath\teamwork.ico"
-    $form.Icon = $InsertIcon
-
-    $InsertGif = (Get-Item -Path "$RootPath\gears.gif")
-    $img = [System.Drawing.Image]::fromfile($InsertGif)
-
-    $box = New-Object Windows.Forms.picturebox -Property @{
-        Size = New-Object System.Drawing.Size(90,90)
-        SizeMode = [System.Windows.Forms.PictureBoxSizeMode]::StretchImage
-        Location = New-Object System.Drawing.Point(185,275)
-        Image = $img
-    }
-    $InsertIcon = New-Object System.Drawing.Icon "$RootPath\teamwork.ico"
-    $form.Icon = $InsertIcon
 
     $shpDivider = New-Object System.Windows.Forms.Label -Property @{
         Location = New-Object System.Drawing.Point(30,50)
@@ -109,14 +211,14 @@ try {
         BorderStyle = 'Fixed3D'
     }
     $lblMainMenu = New-Object System.Windows.Forms.Label -Property @{
-        Location = New-Object System.Drawing.Point(120,30)
-        Size = New-Object System.Drawing.Size(280,22)
-        Font = New-Object System.Drawing.Font("Microsoft Sans Serif",10,[System.Drawing.FontStyle]::Bold)
-        Text = "Group Management Tools"
+        Location = New-Object System.Drawing.Point(110,25)
+        Size = New-Object System.Drawing.Size(280,20)
+        Font = New-Object System.Drawing.Font("Microsoft Sans Serif",9,[System.Drawing.FontStyle]::Bold)
+        Text = "Group Management Tools $EXOStatus"
     }
     $btnCreate = New-Object System.Windows.Forms.Button -Property @{
         Location = New-Object System.Drawing.Point(85,95)
-        Size = New-Object System.Drawing.Size(125,50)
+        Size = New-Object System.Drawing.Size(145,60)
         ForeColor = $json.ToolUILabelColor
         BackColor = $json.ToolUIBtnColor
         Text = 'CREATE'      
@@ -131,33 +233,64 @@ try {
         [System.Windows.MessageBox]::Show("CREATE function completed","$($json.ToolName) $($json.ToolVersion)",$OKButton,$InfoIcon)
     })
 
-
     $btnRead = New-Object System.Windows.Forms.Button -Property @{
         Location = New-Object System.Drawing.Point(250,95)
-        Size = New-Object System.Drawing.Size(125,50)
+        Size = New-Object System.Drawing.Size(145,60)
         ForeColor = $json.ToolUILabelColor
         BackColor = $json.ToolUIBtnColor
         Text = 'READ'      
     }
+     $btnRead.Add_Click({
+        Write-Host "$(Get-Date -Format "HH:mm")[Log]: READ selected"
+        Import-Module "$RootPath\Read.ps1" -Force
+        Write-Host "$(Get-Date -Format "HH:mm")[Log]: READ function imported"
+        ReadDL
+        Write-Host "`n`n$(Get-Date -Format "HH:mm")[Log]: READ function completed"
+        [System.Windows.MessageBox]::Show("READ function completed","$($json.ToolName) $($json.ToolVersion)",$OKButton,$InfoIcon)
+    })
     $btnUpdate = New-Object System.Windows.Forms.Button -Property @{
         Location = New-Object System.Drawing.Point(85,180)
-        Size = New-Object System.Drawing.Size(125,50)
+        Size = New-Object System.Drawing.Size(145,60)
         ForeColor = $json.ToolUILabelColor
         BackColor = $json.ToolUIBtnColor
         Text = 'UPDATE'      
     }
     $btnDelete = New-Object System.Windows.Forms.Button -Property @{
         Location = New-Object System.Drawing.Point(250,180)
-        Size = New-Object System.Drawing.Size(125,50)
+        Size = New-Object System.Drawing.Size(145,60)
         ForeColor = $json.ToolUILabelColor
         BackColor = $json.ToolUIBtnColor
         Text = 'DELETE'      
     }
 
+    If($ConResult -eq "Yes")
+    {
+        SetToolMode -Mode "Enabled"
+    }else{
+        SetToolMode -Mode "Disabled"
+    }
+
     #---form-render
     $form.Controls.Add($lblTOA)
-    $form.Controls.Add($btnStart)
+    $form.Controls.Add($btnStart) 
+    $form.Controls.Add($cbxTOA) 
+    $form.Controls.Add($shpDivider)
+    $form.Controls.Add($lblMainMenu)
+    $form.Controls.Add($btnCreate)
+    $form.Controls.Add($btnRead)
+    $form.Controls.Add($btnUpdate)
+    $form.Controls.Add($btnDelete)
+
+    If($json.ToolShowTOA -eq "True")
+    {
+        ShowScreen -Control "TOA"
+    }else{
+        ShowScreen -Control "MainMenu"
+    }
+    $form.Controls.Add($Gif_CompBackground)
+    $Gif_CompBackground.SendToBack()
     $form.ShowDialog() | Out-Null
+    Write-Host "$(Get-Date -Format "HH:mm")[Log]: Form closed"
     
     Get-Kill 
 }
